f40a0a23125b47934440209dd3c17376c5559afd
x14g010 <x14g010@PC505N010.chiba-fjb.ac.jp>
2015-04-16 15:02:10 +0900

    test

---
+++ src/Format.java
@@ -0,0 +1,12 @@
+
+public class Format {
+
+	public static void main(String[] args) {
+		int[] a = {5,8,2,1,4};
+		int i;
+		for(i=0;i<a.length;i++)
+		{
+		    System.out.println(a[i]);
+		}
+	}}
+
